58cd2b71dfb0094c8b49440f2e5f3d648881ead5
Victoria Riazanova <vikaryazanova94@gmail.com>
2025-02-10 15:16:38 +0300

    Задачи выполнены

--- 6.exception-closure/task.js
+++ 6.exception-closure/task.js
@@ -1,1 +1,59 @@
-﻿
+﻿function parseCount(value) {
+    const parcedValue = Number.parseFloat(value);
+
+    if (isNaN(parcedValue)) {
+        throw new Error("Невалидное значение");
+    } 
+    
+    return parcedValue;
+}
+
+function validateCount(value) {
+    if (value === undefined) {
+        return new Error("Ошибка! Значение не определено.");
+    }
+
+    try {
+        const result = parseCount(value);
+        return result;
+    } catch (error) {
+        return error;
+    }
+}
+
+class Triangle {
+    constructor(a, b, c) {
+        if (a + b <= c || a + c <= b || b + c <= a) {
+            throw new Error("Треугольник с такими сторонами не существует");
+        }
+        
+        this.a = a;
+        this.b = b;
+        this.c = c;
+    }
+
+    get perimeter() {
+        return this.a + this.b + this.c;
+    }
+
+    get area() {
+        const s = this.perimeter / 2;
+        const area = Math.sqrt(s * (s - this.a) * (s - this.b) * (s - this.c));
+        return parseFloat(area.toFixed(3));
+    }
+}
+
+function getTriangle(a, b, c) {
+    try {
+        return new Triangle(a, b, c);
+    } catch (error) {
+        return {
+            get area() {
+                return "Ошибка! Треугольник не существует";
+            },
+            get perimeter() {
+                return "Ошибка! Треугольник не существует";
+            }
+        };
+    }
+}
